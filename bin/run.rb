87987523e36aa06a085ef02a118053b8f5b90263
github username <github email address>
2020-02-11 16:43:46 -0500

    prevent duplicate user

--- bin/run.rb
+++ bin/run.rb
@@ -1,33 +1,36 @@
 require_relative '../config/environment'
 
-
 puts "Welcome  to my User_create Test world!"
 def user_or_company 
-    puts "who are you user? or company?"
-    input = gets.chomp
-    if input == "user"
-        user_input
-    elsif input == "company"
-        company_input
-    else
-    puts "try it again please"
-    end
+    prompt = TTY::Prompt.new
+    input = prompt.select("who are you user? or company?", %w(User Company))
 
+    if input == "User"
+        user_input
+    elsif input == "Company"
+        company_input
+    end
 end
 
 def user_input
-    puts "Enter your name"
-     input_name = gets.chomp
      puts "Enter your email"
      input_email = gets.chomp
-     puts "Enter your contact number"
-     input_contact = gets.chomp
-
-     User.create(name: input_name, email: input_email, contact: input_contact)
+     finder = User.user_find_email(input_email)
+     if finder == nil
+        puts "Enter your name"
+         input_name = gets.chomp
+         puts "Enter your contact number"
+         input_contact = gets.chomp
+    
+         User.create(name: input_name.downcase, email: input_email.downcase, contact: input_contact)
+     else
+        puts "Welcome back #{finder.name}!"
+     end
  end
 
 def company_input
     puts "hello you are in company site"
 end
 
+# user_or_company
 user_or_company
--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+    def self.user_find_email(email)
+        result = User.all.find{|user| user.email == email}
+    end
+
     def create(name: ,email: ,contact: )
         user = User.new(name, email, contact)
         user.save
